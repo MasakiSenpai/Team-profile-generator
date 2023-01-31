051e6a9f97f23f8b0e487e1c5b909b9d19a78f0d
MasakiDesktop <mandij50@gmail.com>
2023-01-31 15:22:43 -0500

    finished classes and got them to generate

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const inquirer = require('inquirer');
 const fs = require('fs')
 const path = require('path');
 
-// const Engineer = require('./lib/Engineer');
-// const Intern = require('./lib/Intern');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 
 const outputFolder = path.resolve(__dirname, 'dist');
@@ -22,6 +22,12 @@ function menu() {
         switch(data.userChoice) {
            case 'Manager':
                 createManger();
+                break;
+            case 'Engineer':
+                createEngineer();
+                break;
+            case 'Intern':
+                createIntern();
                 break;
             default:
                 endProgram();
@@ -62,10 +68,90 @@ function generateManager(emp) {
     return ` 
     <div>
         <h3>Manager</h3>
-        <h5>${emp.getName()}</h5>
+        <h4>${emp.getName()}</h4>
+        <p><b>Id: </b>${emp.getId()}</p>
+        <p><b>Email: </b>${emp.getEmail()}</p>
+        <p><b>Office Number: </b>${emp.getOfficeNumber()}</p>
+    </div>`
+}
+
+function createEngineer() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Employee Name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Employee email?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Employee GitHub?'
+        }
+    ]).then((data) => {
+        const newEngineer = new Engineer(data.name, data.id, data.email, data.github);
+        employees.push(newEngineer);
+        menu();
+    })
+}
+
+function generateEngineer(emp) {
+    return ` 
+    <div>
+        <h3>Engineer</h3>
+        <h4>${emp.getName()}</h4>
+        <p><b>Id: </b>${emp.getId()}</p>
+        <p><b>Email: </b>${emp.getEmail()}</p>
+        <p><b>GitHub: </b>${emp.getGithub()}</p>
+    </div>`
+}
+
+function createIntern() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Employee Name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'Employee ID?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Employee email?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'Employee School?'
+        }
+    ]).then((data) => {
+        const newIntern = new Intern(data.name, data.id, data.email, data.school);
+        employees.push(newIntern);
+        menu();
+    })
+}
+
+function generateIntern(emp) {
+    return ` 
+    <div>
+        <h3>Intern</h3>
+        <h4>${emp.getName()}</h4>
         <p><b>Id:</b>${emp.getId()}</p>
         <p><b>Email:</b>${emp.getEmail()}</p>
-        <p><b>Office Number:</b>${emp.getOfficeNumber()}</p>
+        <p><b>School: </b>${emp.getSchool()}</p>
     </div>`
 }
 
@@ -87,6 +173,9 @@ function generateHtml() {
                 break;
             case 'Engineer':
                 html += generateEngineer(emp);
+                break;
+            case 'Intern':
+                html += generateIntern(emp);
                 break;
         }   
     });
